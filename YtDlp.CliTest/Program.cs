216cc2ecbfeae35f97ae3e22e4cf1b36dcb28ab7
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-06 22:20:29 +0200

    Added YtDlp utility class that helps with downloading and caching audio files + meta data

---
+++ YtDlp.CliTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using YoutubeDLSharp;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: YtDlp.CliTest.exe <cachePath> <url>");
+            return;
+        }
+
+        string cachePath = args[0];
+        string url = args[1];
+
+        var ytDlp = new YtDlp(cachePath);
+        var metaData = await ytDlp.DownloadMetaData(url, default);
+
+        Console.WriteLine($"Video name: {metaData.Data.Title}");
+
+        var result = await ytDlp.DownloadAudioFile(url, default, new OutputDownloadProgress(), new OutputProgress());
+
+        Console.WriteLine($"Success: {result.Success}, Data: {result.Data}, ErrorOutput: {string.Join("\n", result.ErrorOutput)}");
+    }
+}
+
+class OutputDownloadProgress : IProgress<DownloadProgress>
+{
+    public void Report(DownloadProgress value)
+    {
+        Console.WriteLine($"State: {value.State}, Percent: {value.Progress}, Speed: {value.DownloadSpeed}, ETA: {value.ETA}");
+    }
+}
+
+class OutputProgress : IProgress<string>
+{
+    public void Report(string value)
+    {
+        Console.WriteLine(value);
+    }
+}
---
+++ YtDlp/YtDlp.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using HashUtility;
+using Newtonsoft.Json;
+using YoutubeDLSharp;
+using YoutubeDLSharp.Metadata;
+using YoutubeDLSharp.Options;
+
+public class YtDlp
+{
+    private YoutubeDL youtubeDL;
+
+    private readonly string cachePath;
+    private readonly string binariesPath;
+    private readonly string audioFilesPath;
+    private readonly string ytDlpCachePath;
+    private Task? downloadBinariesTask;
+
+    private Dictionary<string, Task> downloadTasks = [];
+
+    public YtDlp(string cachePath, byte maxNumberOfProcesses = 4)
+    {
+        this.cachePath = cachePath;
+        binariesPath = Path.Combine(cachePath, "bin");
+        audioFilesPath = Path.Combine(cachePath, "audio");
+        ytDlpCachePath = Path.Combine(cachePath, "yt-dlp");
+
+        youtubeDL = new(maxNumberOfProcesses)
+        {
+            FFmpegPath = Path.Combine(binariesPath, Utils.FfmpegBinaryName),
+            YoutubeDLPath = Path.Combine(binariesPath, Utils.YtDlpBinaryName),
+            OutputFolder = Path.Combine(cachePath, "audio"),
+        };
+
+        if (!Directory.Exists(binariesPath))
+            Directory.CreateDirectory(binariesPath);
+
+        if (!Directory.Exists(audioFilesPath))
+            Directory.CreateDirectory(audioFilesPath);
+
+        if (!Directory.Exists(ytDlpCachePath))
+            Directory.CreateDirectory(ytDlpCachePath);
+
+        downloadBinariesTask = DownloadBinaries();
+    }
+
+    private async Task DownloadBinaries()
+    {
+        if (downloadBinariesTask != null)
+        {
+            await downloadBinariesTask;
+            return;
+        }
+
+        await Utils.DownloadBinaries(skipExisting: true, directoryPath: binariesPath);
+    }
+
+    public async Task<RunResult<string>> DownloadAudioFile(string url, CancellationToken cancellationToken, IProgress<DownloadProgress>? progress, IProgress<string>? output = null)
+    {
+        await DownloadBinaries();
+
+        var urlHash = HashUrl(url);
+        if (File.Exists(Path.Combine(audioFilesPath, $"{urlHash}.mp3")))
+            return new RunResult<string>(true, error: [], result: Path.Combine(audioFilesPath, $"{urlHash}.mp3"));
+
+        var options = new OptionSet()
+        {
+            CacheDir = ytDlpCachePath,
+            AudioQuality = 3,
+            NoCheckCertificates = true,
+            Output = Path.Combine(audioFilesPath, $"{urlHash}.mp3"),
+        };
+
+        return await youtubeDL.RunAudioDownload(url, AudioConversionFormat.Mp3, cancellationToken, progress, output, options);
+    }
+
+    public async Task<RunResult<VideoData>> DownloadMetaData(string url, CancellationToken cancellationToken)
+    {
+        uint urlHash = HashUrl(url);
+        string filePath = Path.Combine(audioFilesPath, $"{urlHash}.json");
+        RunResult<VideoData> result;
+        if (!File.Exists(Path.Combine(audioFilesPath, $"{urlHash}.json")))
+        {
+            result = await youtubeDL.RunVideoDataFetch(url, cancellationToken);
+
+            if (result.Success)
+            {
+                await File.WriteAllTextAsync(filePath, JsonConvert.SerializeObject(result.Data));
+            }
+        }
+        else
+        {
+            VideoData videoData = JsonConvert.DeserializeObject<VideoData>(await File.ReadAllTextAsync(filePath))!;
+            result = new RunResult<VideoData>(true, error: [], result: videoData);
+        }
+
+        return result;
+    }
+
+    private uint HashUrl(string url)
+    {
+        return url.GetStableHashCode();
+    }
+}
