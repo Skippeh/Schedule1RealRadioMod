d08d76161c94f543cbf4fa84e052a2b1c5462844
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-14 16:05:12 +0200

    Added list item that shows download state and progress (wip)

--- RealRadio/Components/YoutubeDL/YtDlpUiManager.cs
+++ RealRadio/Components/YoutubeDL/YtDlpUiManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ScheduleOne.DevUtilities;
 using ScheduleOne.UI;
 using UnityEngine;
@@ -14,9 +15,15 @@ public class YtDlpUiManager : PersistentSingleton<YtDlpUiManager>
 
     public UIDocument Document { get; private set; } = null!;
 
+    [field: SerializeField]
+    public VisualTreeAsset ListItemAsset { get; private set; } = null!;
+
     private VisualElement downloadIndicator = null!;
     private VisualElement downloadIndicatorSpinIcon = null!;
     private Label noActiveDownloadsLabel = null!;
+    private ScrollView itemsContainer = null!;
+
+    private Dictionary<string, ListItem> items = [];
 
     public bool DownloadIndicatorVisible
     {
@@ -62,6 +69,10 @@ public class YtDlpUiManager : PersistentSingleton<YtDlpUiManager>
         downloadIndicator = Document.rootVisualElement.Query(name: "DownloadIndicator").First() ?? throw new InvalidOperationException("Could not find download indicator ui element");
         downloadIndicatorSpinIcon = downloadIndicator.Query(className: "icon-spinning").First() ?? throw new InvalidOperationException("Could not find download indicator's spin icon ui element");
         noActiveDownloadsLabel = Document.rootVisualElement.Query<Label>(name: "NoActiveDownloadsLabel").First() ?? throw new InvalidOperationException("Could not find no active downloads label ui element");
+        itemsContainer = Document.rootVisualElement.Query<ScrollView>(name: "ListItemsContainer").First() ?? throw new InvalidOperationException("Could not find item list container ui element");
+
+        if (ListItemAsset == null)
+            throw new InvalidOperationException("ListItemAsset is not set");
 
         foreach (var (url, progress) in YtDlpManager.Instance.DownloadProgresses)
             OnDownloadProgressUpdate(url, progress);
@@ -71,6 +82,45 @@ public class YtDlpUiManager : PersistentSingleton<YtDlpUiManager>
 
     private void OnDownloadProgressUpdate(string url, DownloadProgress progress)
     {
+        if (!items.TryGetValue(url, out var item))
+        {
+            VisualElement element = ListItemAsset.Instantiate();
+            itemsContainer.Add(element);
+            item = new ListItem(element);
+            items.Add(url, item);
+
+            var metaData = YtDlpManager.Instance.AudioMetaData[url];
+            item.Name = $"{metaData.Title} ({metaData.Uploader})";
+        }
+
+        item.StateText = progress.State.ToString();
+
+        switch (progress.State)
+        {
+            case DownloadState.PreProcessing:
+                item.Progress = 0f;
+                break;
+            case DownloadState.Downloading:
+                if (progress.Progress > 0f)
+                    item.Progress = progress.Progress * 0.9f;
+
+                break;
+            case DownloadState.Success:
+                item.Progress = 1f;
+                break;
+            case DownloadState.Error:
+                item.Progress = 1f;
+                break;
+            default:
+                item.Progress = 0.9f;
+                break;
+        }
+
+        if (progress.State == DownloadState.Success)
+        {
+            items.Remove(url);
+            itemsContainer.Remove(item.Element);
+        }
     }
 
     private void Update()
@@ -84,6 +134,43 @@ public class YtDlpUiManager : PersistentSingleton<YtDlpUiManager>
         if (inGameAndNotPaused)
         {
             downloadIndicatorSpinIcon.style.rotate = new StyleRotate(new UnityEngine.UIElements.Rotate(new Angle((float)(Time.realtimeSinceStartupAsDouble * LoadingIconSpinDegreesPerSecond % 360d), AngleUnit.Degree)));
+        }
+    }
+
+    class ListItem
+    {
+        public string Name
+        {
+            get => name.text;
+            set => name.text = value;
+        }
+
+        public float Progress
+        {
+            get => progressBar.value;
+            set => progressBar.value = value;
+        }
+
+        public string StateText
+        {
+            get => state.text;
+            set => state.text = value;
+        }
+
+        public VisualElement Element { get; private set; }
+
+        private Label name;
+        private Label state;
+        private ProgressBar progressBar;
+
+        public ListItem(VisualElement element)
+        {
+            Element = element;
+
+            name = element.Query<Label>(name: "Name").First() ?? throw new InvalidOperationException("Could not find name ui element");
+            state = element.Query<Label>(name: "State").First() ?? throw new InvalidOperationException("Could not find state ui element");
+            progressBar = element.Query<ProgressBar>(name: "ProgressBar").First() ?? throw new InvalidOperationException("Could not find progress bar ui element");
+            progressBar.value = 0f;
         }
     }
 }
