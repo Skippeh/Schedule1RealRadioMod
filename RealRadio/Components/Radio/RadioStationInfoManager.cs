e3f20a2147fc6f082a05b25ee8b89ca8e663ebcb
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-19 19:22:58 +0200

    Added support for getting current song name from YtDlp type stations

--- RealRadio/Components/Radio/RadioStationInfoManager.cs
+++ RealRadio/Components/Radio/RadioStationInfoManager.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using RealRadio.Components.API.Data;
+using RealRadio.Components.YoutubeDL;
+using RealRadio.Data;
 using ScheduleOne.DevUtilities;
 using SongInfoFetcher;
 using SongInfoFetcher.GlobalPlayer;
@@ -17,14 +18,16 @@ public class RadioStationInfoManager : PersistentSingleton<RadioStationInfoManag
 {
     public static Action<SongInfoFetchManager>? RegisterFetchers;
 
-    public Action<Data.RadioStation, SongInfo>? SongInfoUpdated { get; set; }
+    public Action<RadioStation, SongInfo>? SongInfoUpdated { get; set; }
 
     public SongInfoFetchManager SongInfoFetchManager { get; private set; } = null!;
 
-    private readonly Dictionary<Data.RadioStation, ISongInfoFetcher> fetchers = [];
-    private Dictionary<Data.RadioStation, List<SongInfo>> pendingUpdates = [];
+    private readonly Dictionary<RadioStation, ISongInfoFetcher> fetchers = [];
+    private readonly Dictionary<RadioStation, ManualSongInfoFetcher> manualFetchers = [];
+    private Dictionary<RadioStation, List<SongInfo>> pendingUpdates = [];
     private readonly Dictionary<ISongInfoFetcher, float> fetchersToPoll = [];
     private readonly HashSet<ISongInfoFetcher> updatedPollTimes = [];
+    private bool subscribedToSyncManager;
 
     public override void Awake()
     {
@@ -51,6 +54,16 @@ public class RadioStationInfoManager : PersistentSingleton<RadioStationInfoManag
 
     private void Update()
     {
+        if (!subscribedToSyncManager && RadioSyncManager.InstanceExists)
+        {
+            RadioSyncManager.Instance.OnStateReceived += OnRadioStationSyncStateReceived;
+            subscribedToSyncManager = true;
+        }
+        else if (subscribedToSyncManager && !RadioSyncManager.InstanceExists)
+        {
+            subscribedToSyncManager = false;
+        }
+
         updatedPollTimes.Clear();
 
         foreach (var kv in fetchersToPoll)
@@ -84,10 +97,18 @@ public class RadioStationInfoManager : PersistentSingleton<RadioStationInfoManag
         }
     }
 
+    private void OnRadioStationSyncStateReceived(RadioStation station, RadioStationState state)
+    {
+        if (!manualFetchers.TryGetValue(station, out var fetcher))
+            return;
+
+        fetcher.CurrentSong = GetSongFromState(station, state);
+    }
+
     /// <summary>
     /// Requests song info for the specified radio station.
     /// </summary>
-    public async Task<SongInfo?> RequestSong(Data.RadioStation station)
+    public async Task<SongInfo?> RequestSong(RadioStation station)
     {
         if (!fetchers.TryGetValue(station, out var fetcher))
             return null;
@@ -105,28 +126,32 @@ public class RadioStationInfoManager : PersistentSingleton<RadioStationInfoManag
 
     /// <summary>
     /// Returns the current cached song info for the specified radio station. If no song info is available, returns null.
-    /// Use <see cref="RequestSong(Data.RadioStation)"/> to request song info.
+    /// Use <see cref="RequestSong(RadioStation)"/> to request song info.
     /// /// </summary>
-    public SongInfo? GetSong(Data.RadioStation station) => fetchers.TryGetValue(station, out var fetcher) ? fetcher.CurrentSong : null;
+    public SongInfo? GetSong(RadioStation station) => fetchers.TryGetValue(station, out var fetcher) ? fetcher.CurrentSong : null;
 
-    private void OnRadioStationAdded(Data.RadioStation station)
+    private void OnRadioStationAdded(RadioStation station)
     {
-        if (station.Type != RadioType.InternetRadio)
-            return;
-
-        // todo: initialize fetcher
-        StartCoroutine(AddFetcher(station));
-    }
-
-    private void OnRadioStationRemoved(Data.RadioStation station)
-    {
-        if (fetchers.Remove(station, out var fetcher))
+        switch (station.Type)
         {
-            StartCoroutine(RemoveFetcher(station));
+            case RadioType.InternetRadio:
+                StartCoroutine(AddInternetRadioFetcher(station));
+                break;
+            case RadioType.YtDlp:
+                AddYtDlpRadioFetcher(station);
+                break;
+            default:
+                Plugin.Logger.LogWarning($"Can not create song info fetcher due to unexpected radio type: {station.Type}");
+                break;
         }
     }
 
-    private IEnumerator AddFetcher(Data.RadioStation station)
+    private void OnRadioStationRemoved(RadioStation station)
+    {
+        StartCoroutine(RemoveFetcher(station));
+    }
+
+    private IEnumerator AddInternetRadioFetcher(RadioStation station)
     {
         var getFetcherTask = SongInfoFetchManager.GetFetcher(new Uri(station.Url));
         yield return new WaitUntil(() => getFetcherTask.IsCompleted);
@@ -165,12 +190,69 @@ public class RadioStationInfoManager : PersistentSingleton<RadioStationInfoManag
             fetcher.RequestSongInfo();
     }
 
-    private IEnumerator RemoveFetcher(Data.RadioStation station)
+    private void AddYtDlpRadioFetcher(RadioStation station)
     {
-        var removeTask = SongInfoFetchManager.RemoveFetcher(new Uri(station.Url));
-        yield return new WaitUntil(() => removeTask.IsCompleted);
+        var fetcher = new ManualSongInfoFetcher();
+        fetchers[station] = fetcher;
+        manualFetchers[station] = fetcher;
 
-        if (removeTask.IsFaulted)
-            Plugin.Logger.LogError($"Failed to remove song info fetcher for radio station '{station.Id} ({station.Url})':\n{removeTask.Exception}");
+        fetcher.SubscribeToSongInfoChanges(songInfo =>
+        {
+            lock (pendingUpdates)
+            {
+                if (!pendingUpdates.TryGetValue(station, out var list))
+                {
+                    list = new List<SongInfo>();
+                    pendingUpdates[station] = list;
+                }
+
+                list.Add(songInfo);
+            }
+        });
+
+        var state = RadioSyncManager.Instance?.GetLocalState(station);
+
+        if (state != null)
+            fetcher.CurrentSong = GetSongFromState(station, state);
+    }
+
+    private IEnumerator RemoveFetcher(RadioStation station)
+    {
+        fetchers.Remove(station);
+
+        // if the fetcher is not in the manual fetchers, it was added to the song info fetch manager
+        if (!manualFetchers.Remove(station))
+        {
+            var removeTask = SongInfoFetchManager.RemoveFetcher(new Uri(station.Url));
+            yield return new WaitUntil(() => removeTask.IsCompleted);
+
+            if (removeTask.IsFaulted)
+                Plugin.Logger.LogError($"Failed to remove song info fetcher for radio station '{station.Id} ({station.Url})':\n{removeTask.Exception}");
+        }
+    }
+
+    private SongInfo? GetSongFromState(RadioStation station, RadioStationState state)
+    {
+        if (!state.IsValid())
+            return null;
+
+        if (station.Type != RadioType.YtDlp || station.Urls == null || station.Urls.Length < state.SongIndex)
+            return null;
+
+        string url = station.Urls[state.SongIndex.Value];
+
+        if (!YtDlpManager.Instance.AudioMetaData.TryGetValue(url, out var metaData))
+            return null;
+
+        string? title = metaData.Title;
+        int indexOfDash = title.IndexOf('-');
+
+        if (indexOfDash == -1)
+            return new SongInfo(title, metaData.Uploader);
+
+        string artistName = title.Substring(0, indexOfDash).Trim();
+        title = indexOfDash >= title.Length ? null : title.Substring(indexOfDash + 1).Trim();
+
+        return new SongInfo(title, artistName);
     }
 }
