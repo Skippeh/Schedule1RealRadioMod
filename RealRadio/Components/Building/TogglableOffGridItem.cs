76085920ebb065141befb9660e62786e7b770200
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-21 18:17:28 +0200

    Cleaned up some dead code

--- RealRadio/Components/Building/TogglableOffGridItem.cs
+++ RealRadio/Components/Building/TogglableOffGridItem.cs
@@ -15,16 +15,6 @@ public abstract class TogglableOffGridItem : OffGridItem
     [field: SyncVar(Channel = FishNet.Transporting.Channel.Reliable, ReadPermissions = ReadPermission.ExcludeOwner, WritePermissions = WritePermission.ServerOnly, OnChange = nameof(OnStateToggled))]
     public bool IsOn { get; [ServerRpc(RequireOwnership = false, RunLocally = true)] set; }
 
-    public override void Awake()
-    {
-        base.Awake();
-    }
-
-    private void OnInteractStart()
-    {
-        IsOn = !IsOn;
-    }
-
     protected virtual void OnStateToggled(bool prev, bool next, bool asServer)
     {
     }
