2e29a524f1ec1e7536df35ab7fcf715517595e0f
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-23 15:46:33 +0200

    Use less verbose BackgroundPosition assignments

--- RealRadio/Components/UI/Phone/RadioAppUi.cs
+++ RealRadio/Components/UI/Phone/RadioAppUi.cs
@@ -30,8 +30,8 @@ public class RadioAppUi : MonoBehaviour
 
     private void RandomizeBackgroundParameters()
     {
-        root.style.backgroundPositionX = new StyleBackgroundPosition(new BackgroundPosition(BackgroundPositionKeyword.Left, new Length(UnityEngine.Random.Range(-1000f, 1000f))));
-        root.style.backgroundPositionY = new StyleBackgroundPosition(new BackgroundPosition(BackgroundPositionKeyword.Top, new Length(UnityEngine.Random.Range(-1000f, 1000f))));
+        root.style.backgroundPositionX = new BackgroundPosition(BackgroundPositionKeyword.Left, UnityEngine.Random.Range(-1000f, 1000f));
+        root.style.backgroundPositionY = new BackgroundPosition(BackgroundPositionKeyword.Top, UnityEngine.Random.Range(-1000f, 1000f));
 
         float direction = UnityEngine.Random.Range(0f, 360f);
         backgroundMoveDirection = new Vector2(Mathf.Cos(direction * Mathf.Deg2Rad), Mathf.Sin(direction * Mathf.Deg2Rad)).normalized;
@@ -47,7 +47,7 @@ public class RadioAppUi : MonoBehaviour
         float x = root.resolvedStyle.backgroundPositionX.offset.m_Value;
         float y = root.resolvedStyle.backgroundPositionY.offset.m_Value;
 
-        root.style.backgroundPositionX = new StyleBackgroundPosition(new BackgroundPosition(BackgroundPositionKeyword.Left, new Length(x + (backgroundScrollSpeed * Time.unscaledDeltaTime * backgroundMoveDirection.x))));
-        root.style.backgroundPositionY = new StyleBackgroundPosition(new BackgroundPosition(BackgroundPositionKeyword.Top, new Length(y + (backgroundScrollSpeed * Time.unscaledDeltaTime * backgroundMoveDirection.y))));
+        root.style.backgroundPositionX = new BackgroundPosition(BackgroundPositionKeyword.Left, x + (backgroundScrollSpeed * Time.unscaledDeltaTime * backgroundMoveDirection.x));
+        root.style.backgroundPositionY = new BackgroundPosition(BackgroundPositionKeyword.Top, y + (backgroundScrollSpeed * Time.unscaledDeltaTime * backgroundMoveDirection.y));
     }
 }
