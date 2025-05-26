491323b5bbfc289b7e2fd92c99422f799ded68ba
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-27 00:26:18 +0200

    Added more fields

--- RealRadio/Components/UI/Phone/UIElements/StationProperties.cs
+++ RealRadio/Components/UI/Phone/UIElements/StationProperties.cs
@@ -32,8 +32,10 @@ public class StationProperties
         }
     }
 
-    private TextField nameInput;
+    private TextField nameField;
     private EnumField typeField;
+    private TextField abbreviationField;
+    private Toggle canBePlayedByNPCsToggle;
 
     private RadioStation? station;
     private bool readOnly;
@@ -43,12 +45,20 @@ public class StationProperties
 
     public StationProperties(VisualElement root)
     {
-        nameInput = root.Query<TextField>(name: "Name").First() ?? throw new InvalidOperationException("Could not find name TextField ui element");
-        stationChanged += () => nameInput.text = Station?.Name ?? string.Empty;
-        readOnlyChanged += () => nameInput.SetEnabled(!ReadOnly);
+        nameField = root.Query<TextField>(name: "Name").First() ?? throw new InvalidOperationException("Could not find name TextField ui element");
+        stationChanged += () => nameField.text = Station?.Name ?? string.Empty;
+        readOnlyChanged += () => nameField.SetEnabled(!ReadOnly);
 
         typeField = root.Query<EnumField>(name: "Type").First() ?? throw new InvalidOperationException("Could not find type EnumField ui element");
         stationChanged += () => typeField.value = Station?.Type ?? 0;
         readOnlyChanged += () => typeField.SetEnabled(!ReadOnly);
+
+        abbreviationField = root.Query<TextField>(name: "Abbreviation").First() ?? throw new InvalidOperationException("Could not find abbreviation TextField ui element");
+        stationChanged += () => abbreviationField.text = Station?.Abbreviation ?? string.Empty;
+        readOnlyChanged += () => abbreviationField.SetEnabled(!ReadOnly);
+
+        canBePlayedByNPCsToggle = root.Query<Toggle>(name: "CanBePlayedByNPCs").First() ?? throw new InvalidOperationException("Could not find can be played by NPCs Toggle ui element");
+        stationChanged += () => canBePlayedByNPCsToggle.value = Station?.CanBePlayedByNPCs ?? false;
+        readOnlyChanged += () => canBePlayedByNPCsToggle.SetEnabled(!ReadOnly);
     }
 }
