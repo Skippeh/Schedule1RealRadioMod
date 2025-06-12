ce753f148e709067666b091e2d2585de8410cc5c
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-06-12 19:20:16 +0200

    Replaced portable radio with a custom model and removed input controls
    Model is temporary but the goal is to have all features implemented and then make it look good

--- RealRadio/Components/Building/Buildables/AnalogRadio.cs
+++ RealRadio/Components/Building/Buildables/AnalogRadio.cs
@@ -1,36 +1,12 @@
-using System;
 using FishNet.Object;
-using HashUtility;
-using RealRadio.Components.Radio;
-using RealRadio.Components.UI.WorldUI;
-using ScheduleOne;
-using ScheduleOne.PlayerScripts;
-using ScheduleOne.UI;
-using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace RealRadio.Components.Building.Buildables;
 
 public class AnalogRadio : Radio
 {
-    [SerializeField] private DraggableSlider stationEditSlider = null!;
-    [SerializeField] private DraggableSlider volumeEditSlider = null!;
-
     public override void Awake()
     {
         base.Awake();
-
-        if (stationEditSlider == null)
-            throw new InvalidOperationException("StationEditSlider is null");
-
-        if (volumeEditSlider == null)
-            throw new InvalidOperationException("VolumeEditSlider is null");
-
-        stationEditSlider.LoopedAround.AddListener(OnSliderLoopedAround);
-        volumeEditSlider.ValueChanged.AddListener(OnVolumeSliderValueChanged);
-
-        stationEditSlider.gameObject.SetActive(false);
-        volumeEditSlider.gameObject.SetActive(false);
     }
 
     public override void Start()
@@ -40,31 +16,7 @@ public class AnalogRadio : Radio
         if (isGhost)
             return;
 
-        volumeEditSlider.Value = Volume * volumeEditSlider.MaxValue;
-    }
-
-    private void OnSliderLoopedAround(float direction)
-    {
-        var indexOfStation = RadioStation == null ? 0 : RadioStationManager.Instance.SortedStations.IndexOf(RadioStation);
-
-        int nextIndex = indexOfStation + (int)direction;
-        nextIndex = (nextIndex + RadioStationManager.Instance.SortedStations.Count) % RadioStationManager.Instance.SortedStations.Count;
-
-        if (nextIndex < 0)
-            nextIndex += RadioStationManager.Instance.SortedStations.Count;
-
-        var nextStation = RadioStationManager.Instance.SortedStations[nextIndex];
-        SetRadioStationIdHash(nextStation.Id!.GetStableHashCode());
-    }
-
-    private void OnVolumeSliderValueChanged(float value)
-    {
-        Volume = value / volumeEditSlider.MaxValue;
-
-        if (value <= 0 && IsOn)
-            IsOn = false;
-        else if (value > 0 && !IsOn)
-            IsOn = true;
+        //volumeEditSlider.Value = Volume * volumeEditSlider.MaxValue;
     }
 
     protected override void OnPlayerUserChanged(NetworkObject prev, NetworkObject next, bool asServer)
@@ -73,8 +25,8 @@ public class AnalogRadio : Radio
 
         if (!asServer)
         {
-            stationEditSlider.gameObject.SetActive(next == Player.Local.NetworkObject);
-            volumeEditSlider.gameObject.SetActive(next == Player.Local.NetworkObject);
+            //stationEditSlider.gameObject.SetActive(next == Player.Local.NetworkObject);
+            //volumeEditSlider.gameObject.SetActive(next == Player.Local.NetworkObject);
         }
     }
 }
