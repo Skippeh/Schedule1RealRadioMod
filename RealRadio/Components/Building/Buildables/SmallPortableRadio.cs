c190993b11d7760384063cc91be6f35d7a03d779
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-06-16 16:47:11 +0200

    Added class that handle screen ui on radio - only updates time text for now

--- RealRadio/Components/Building/Buildables/SmallPortableRadio.cs
+++ RealRadio/Components/Building/Buildables/SmallPortableRadio.cs
@@ -1,4 +1,10 @@
+using System;
+using FishNet.Managing.Timing;
 using FishNet.Object;
+using ScheduleOne.GameTime;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TimeManager = ScheduleOne.GameTime.TimeManager;
 
 namespace RealRadio.Components.Building.Buildables;
 
@@ -28,5 +34,48 @@ public class SmallPortableRadio : Radio
             //stationEditSlider.gameObject.SetActive(next == Player.Local.NetworkObject);
             //volumeEditSlider.gameObject.SetActive(next == Player.Local.NetworkObject);
         }
+    }
+}
+
+public class SmallPortableRadioScreenUI : MonoBehaviour
+{
+#nullable disable
+    private SmallPortableRadio radio;
+    private UIDocument document;
+
+    private Label timeLabel;
+    private Label stationNameLabel;
+    private Label artistLabel;
+    private Label titleLabel;
+    private VisualElement logoContainer;
+#nullable enable
+
+    private void Awake()
+    {
+        radio = GetComponentInParent<SmallPortableRadio>() ?? throw new InvalidOperationException("No Radio component found in self or parents");
+        document = GetComponent<UIDocument>() ?? throw new InvalidOperationException("No UIDocument component found on game object");
+    }
+
+    private void OnEnable()
+    {
+        var root = document.rootVisualElement;
+        timeLabel = root.Query<Label>(name: "Time").First() ?? throw new InvalidOperationException("Could not find time label ui element");
+        stationNameLabel = root.Query<Label>(name: "StationName").First() ?? throw new InvalidOperationException("Could not find station name label ui element");
+        artistLabel = root.Query<Label>(name: "SongArtist").First() ?? throw new InvalidOperationException("Could not find artist label ui element");
+        titleLabel = root.Query<Label>(name: "SongTitle").First() ?? throw new InvalidOperationException("Could not find title label ui element");
+        logoContainer = root.Query(name: "Logo").First() ?? throw new InvalidOperationException("Could not find logo container ui element");
+
+        TimeManager.Instance.onMinutePass += OnMinutePass;
+    }
+
+    private void OnDisable()
+    {
+        TimeManager.Instance.onMinutePass -= OnMinutePass;
+    }
+
+    private void OnMinutePass()
+    {
+        int currentTime = TimeManager.Instance.CurrentTime;
+        timeLabel.text = TimeManager.Get12HourTime(currentTime, appendDesignator: true);
     }
 }
