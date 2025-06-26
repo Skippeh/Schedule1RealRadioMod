229729f69e67f4927d697f5a1740df3538d97829
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-06-26 15:02:17 +0200

    Added sound effects and local position offset to button clicks

--- RealRadio/Components/UI/WorldUI/Button.cs
+++ RealRadio/Components/UI/WorldUI/Button.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using ScheduleOne;
+using ScheduleOne.Audio;
 using ScheduleOne.PlayerScripts;
 using UnityEngine;
 
@@ -12,21 +13,45 @@ public class Button : MonoBehaviour
     public event Action? CursorDown;
     public event Action? CursorUp;
 
+    public Vector3 ClickOffset = -Vector3.forward * 0.01f;
+
+    public AudioSourceController? CursorDownSound;
+    public AudioSourceController? CursorUpSound;
+
     private bool cursorIsPushedDown;
 
     private RaycastHit[] hits = new RaycastHit[2];
     private Collider collider = null!;
 
+    private Vector3 initialPosition;
+
     void Awake()
     {
         collider = GetComponent<Collider>();
+        initialPosition = transform.localPosition;
+
+        CursorDown += OnCursorDown;
+        CursorUp += OnCursorUp;
+    }
+
+    private void OnCursorDown()
+    {
+        transform.localPosition = initialPosition + ClickOffset;
+
+        CursorUpSound?.Stop();
+        CursorDownSound?.Play();
+    }
+
+    private void OnCursorUp()
+    {
+        transform.localPosition = initialPosition;
+
+        CursorDownSound?.Stop();
+        CursorUpSound?.Play();
     }
 
     private void Update()
     {
-        if (cursorIsPushedDown && !GameInput.GetButton(GameInput.ButtonCode.PrimaryClick))
-            cursorIsPushedDown = false;
-
         if (GameInput.GetButtonDown(GameInput.ButtonCode.PrimaryClick))
         {
             if (!TestRayHit())
@@ -42,6 +67,9 @@ public class Button : MonoBehaviour
 
             cursorIsPushedDown = false;
         }
+
+        if (cursorIsPushedDown && !GameInput.GetButton(GameInput.ButtonCode.PrimaryClick))
+            cursorIsPushedDown = false;
     }
 
     private bool TestRayHit()
