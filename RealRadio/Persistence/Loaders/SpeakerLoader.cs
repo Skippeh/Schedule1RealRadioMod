a64ca91c473d65ce3ff098f5f2b78cd98d0003b8
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-07-14 05:22:29 +0200

    Implemented Speaker loader

--- RealRadio/Persistence/Loaders/SpeakerLoader.cs
+++ RealRadio/Persistence/Loaders/SpeakerLoader.cs
@@ -1,4 +1,6 @@
+using System;
 using RealRadio.Components.Building.Buildables;
+using ScheduleOne.Persistence.Datas;
 
 namespace RealRadio.Persistence.Loaders;
 
@@ -6,4 +8,27 @@ public class SpeakerLoader<TSpeaker, TSpeakerData> : OffGridItemLoader<TSpeaker,
     where TSpeaker : Speaker
     where TSpeakerData : SpeakerData
 {
+    public override void Load(DynamicSaveData data)
+    {
+        base.Load(data);
+
+        if (Item == null || Data == null)
+            return;
+
+        Item.SelectedAudioChannel = Data.Channel;
+        Item.StereoOutput = Data.StereoOutput;
+        Item.MountRotation = Data.MountRotation;
+
+        if (Data.MasterGuid != null)
+        {
+            var guid = Guid.TryParse(Data.MasterGuid, out Guid masterGuid) ? masterGuid : throw new ArgumentException($"Invalid radio guid in save data: {Data.MasterGuid}");
+
+            var master = GUIDManager.GetObject<Radio>(guid);
+
+            if (master == null)
+                Plugin.Logger.LogWarning($"Could not find radio while loading speaker. Radio guid: {Data.MasterGuid}");
+
+            Item.MasterGuid = masterGuid;
+        }
+    }
 }
