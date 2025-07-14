ae8efd1a3be0e2369b6efa85374e2ad50c4b46d9
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-07-14 05:20:09 +0200

    Changed save data master guid to string since unity json serialization sucks

--- RealRadio/Persistence/Data/SpeakerData.cs
+++ RealRadio/Persistence/Data/SpeakerData.cs
@@ -9,7 +9,7 @@ namespace RealRadio.Persistence.Loaders;
 [Serializable]
 public class SpeakerData(Guid guid, ItemInstance item, int loadOrder, Vector3 position, Vector3 rotation, Guid? masterGuid, Speaker.AudioChannel channel, bool stereoOutput, Vector2 mountRotation) : OffGridItemData(guid, item, loadOrder, position, rotation)
 {
-    public Guid MasterGuid = masterGuid ?? Guid.Empty;
+    public string? MasterGuid = masterGuid?.ToString();
     public Speaker.AudioChannel Channel = channel;
     public bool StereoOutput = stereoOutput;
     public Vector2 MountRotation = mountRotation;
