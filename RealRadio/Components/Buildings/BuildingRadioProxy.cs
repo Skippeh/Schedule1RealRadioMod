95599ebd58942848b16f5322943967d6c2657899
Skipcast <420685+Skippeh@users.noreply.github.com>
2026-03-15 19:34:16 +0100

    More fixes related to game update

--- RealRadio/Components/Buildings/BuildingRadioProxy.cs
+++ RealRadio/Components/Buildings/BuildingRadioProxy.cs
@@ -181,7 +181,7 @@ public class BuildingRadioProxy : RadioProxy
         audioClientObject.SetActive(false);
 
         // Disable existing ambient loops in building
-        var ambientSounds = Building.GetComponentsInChildren<AmbientTrackGroup>(includeInactive: true);
+        var ambientSounds = Building.GetComponentsInChildren<AudioClipListPlayer>(includeInactive: true);
 
         foreach (var ambientSound in ambientSounds)
         {
